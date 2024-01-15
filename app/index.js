97eae8d70ef739ea85a12337bfd6f97cdaf35004
Samuel Cheung <samuelcheung@Samuels-MacBook-Air.local>
2024-01-15 22:58:17 +0800

    extract floating button

--- app/index.js
+++ app/index.js
@@ -2,7 +2,7 @@ import { View, ScrollView, SafeAreaView, Pressable, Text } from "react-native";
 import { Stack, useRouter } from "expo-router";
 import { COLOURS, FONT_SIZE } from "./constants";
 import Dashboard from "@components/dashboard/Dashboard";
-import { floatingButtonStyles } from "./styles";
+import { FloatingButton } from "@components/common/FloatingButton";
 
 const Home = () => {
   const router = useRouter();
@@ -24,21 +24,7 @@ const Home = () => {
       <ScrollView showsVerticalScrollIndicator={false}>
         <Dashboard />
       </ScrollView>
-      <Pressable
-        style={floatingButtonStyles}
-        onPress={() => router.push("/createGroup")}
-      >
-        <Text
-          style={{
-            flex: 1,
-            textAlign: "center",
-            color: COLOURS.white2,
-            fontWeight: 500,
-          }}
-        >
-          Create group
-        </Text>
-      </Pressable>
+      <FloatingButton text={"Create group"} route={"/createGroup"} />
     </SafeAreaView>
   );
 };
