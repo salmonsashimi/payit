fc89f6590007409549349e341f2a28316835eb96
Samuel Cheung <samuelcheung@Samuels-MacBook-Air.local>
2024-01-19 15:50:44 +0800

    add empty group message

---
+++ components/groupInfo/_NoExpenseMessage.jsx
@@ -0,0 +1,9 @@
+import { View, Text } from "react-native";
+
+export const NoExpenseMessage = () => {
+  return (
+    <View>
+      <Text style={{ textAlign: "center" }}>There are no expenses here. </Text>
+    </View>
+  );
+};
